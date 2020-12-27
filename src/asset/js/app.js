37b764a1de16435d20c9420070f93c2f7da03191
Takanori Isobe <t.isobe@snowrobin.jp>
2020-12-28 02:51:40 +0900

    faqページの追加

--- src/asset/js/app.js
+++ src/asset/js/app.js
@@ -9,3 +9,13 @@ document.addEventListener('DOMContentLoaded', function() {
     initSwiper;
     initModal();
 });
+
+var accordionTrigger = document.querySelectorAll('.js-accordion-trigger');
+var accordionElem = document.querySelectorAll('.js-accordion-element');
+
+for (let i = 0; i < accordionTrigger.length; i++) {
+    accordionTrigger[i].addEventListener('click', () => {
+        accordionTrigger[i].classList.toggle('is-active');
+        accordionElem[i].classList.toggle('is-active');
+    });
+}
