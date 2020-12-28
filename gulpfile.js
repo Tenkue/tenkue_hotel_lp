5a426ead88e288cd45b759a0b2f5136ee5befebe
Takanori Isobe <t.isobe@snowrobin.jp>
2020-12-28 11:22:25 +0900

    ビルドコマンドの変更

--- gulpfile.js
+++ gulpfile.js
@@ -202,3 +202,8 @@ exports.default = gulp.series(
     sync,
     watchFile
 );
+
+exports.build = gulp.series(
+    clean,
+    gulp.parallel(htmlMin, ejsCompile, sassCompile, imageMin, jsBundle),
+);
